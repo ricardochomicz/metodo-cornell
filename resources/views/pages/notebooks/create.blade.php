19309dbec587f2850a5cdc159263b501eccc1e71
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2025-04-08 13:52:59 -0300

    refactor: form notebooks

--- resources/views/pages/notebooks/create.blade.php
+++ resources/views/pages/notebooks/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <x-breadcrumb :links="[['label' => 'Cadernos', 'url' => '/notebooks'], ['label' => 'Novo Caderno']]" />
+    <x-breadcrumb :links="[['label' => 'Cadernos', 'url' => '/notebooks'], ['label' => 'Novo Caderno', 'url' => '']]" />
 
     <form class="mt-5 bg-gray-50 p-6 rounded-lg shadow-md max-w-md mx-auto" action="{{ route('notebooks.store') }}"
         method="POST">
